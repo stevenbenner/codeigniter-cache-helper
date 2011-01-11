2425fe5e2f506036322eed4e9e8ce4eb01c074ea
Steven Benner <admin@stevenbenner.com>
2011-01-10 20:01:32 -0800

    Moved the path building code into separate functions.

--- helpers/cache_helper.php
+++ helpers/cache_helper.php
@@ -9,10 +9,62 @@
  * @category	Helpers
  * @author		Steven Benner
  * @link		http://stevenbenner.com/2010/12/caching-with-codeigniter-zen-headaches-and-perfomance/
- * @version		1.2
+ * @version		1.3
  */
 
 // NOTE: This code is for CodeIgniter 2.0. For CI < 1.7.x replace APPPATH with BASEPATH
+
+/**
+ * Get Cache File
+ *
+ * Gets the path to a cache file for the specified uri_string().
+ *
+ * @author	Steven Benner
+ * @param	string	Full uri_string() of the target page (e.g. '/blog/comments/123')
+ * @return	string	Path to the cache file.
+ */
+if ( ! function_exists('get_cache_file'))
+{
+	function get_cache_file($uri_string)
+	{
+		$CI =& get_instance();
+
+		$path = $CI->config->item('cache_path');
+
+		$cache_path = ($path == '') ? APPPATH . 'cache/' : $path;
+
+		$uri =  $CI->config->item('base_url') .
+				$CI->config->item('index_page') .
+				$uri_string;
+
+		return $cache_path . md5($uri);
+	}
+}
+
+/**
+ * Get All Cache Files
+ *
+ * Gets the path to every cache file currently saved.
+ *
+ * @author	Steven Benner
+ * @return	array	Array of cache file paths.
+ */
+if ( ! function_exists('get_all_cache_files'))
+{
+	function get_all_cache_files()
+	{
+		$CI =& get_instance();
+
+		$CI->load->helper('file');
+
+		$path = $CI->config->item('cache_path');
+
+		$cache_path = ($path == '') ? APPPATH . 'cache/' : $path;
+
+		return get_filenames($cache_path);
+	}
+}
+
 
 /**
  * Delete Cache File
@@ -27,16 +79,7 @@ if ( ! function_exists('delete_cache'))
 {
 	function delete_cache($uri_string)
 	{
-		$CI =& get_instance();
-
-		$path = $CI->config->item('cache_path');
-		$cache_path = ($path == '') ? APPPATH . 'cache/' : $path;
-
-		$uri =  $CI->config->item('base_url') .
-				$CI->config->item('index_page') .
-				$uri_string;
-
-		$cache_path .= md5($uri);
+		$cache_path = get_cache_file($uri_string);
 
 		if (file_exists($cache_path))
 		{
@@ -61,14 +104,7 @@ if ( ! function_exists('delete_all_cache'))
 {
 	function delete_all_cache()
 	{
-		$CI =& get_instance();
-
-		$CI->load->helper('file');
-
-		$path = $CI->config->item('cache_path');
-		$cache_path = ($path == '') ? APPPATH . 'cache/' : $path;
-
-		$cache_files = get_filenames($cache_path);
+		$cache_files = get_all_cache_files();
 
 		foreach ($cache_files as $file)
 		{
@@ -95,16 +131,7 @@ if ( ! function_exists('get_cache_expiration'))
 {
 	function get_cache_expiration($uri_string)
 	{
-		$CI =& get_instance();
-
-		$path = $CI->config->item('cache_path');
-		$cache_path = ($path == '') ? APPPATH . 'cache/' : $path;
-
-		$uri =  $CI->config->item('base_url') .
-				$CI->config->item('index_page') .
-				$uri_string;
-
-		$cache_path .= md5($uri);
+		$cache_path = get_cache_file($uri_string);
 
 		if ( ! @file_exists($cache_path))
 		{
